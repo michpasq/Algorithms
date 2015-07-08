9c224023204a3c20c96bb746793ce26383389bab
michpasq <michpasq@umich.edu>
2015-07-07 22:47:11 -0400

    Added some neat c++11 features... but still not done with algorithm

--- InterviewProblems/mergeRanges.cpp
+++ InterviewProblems/mergeRanges.cpp
@@ -5,6 +5,7 @@
 struct timeTuple {
 	int startTime;
 	int endTime;
+	timeTuple(int a, int b) : startTime(a), endTime(b) {};
 };
 
 bool timeTupleComp(const timeTuple& a, const timeTuple& b) {
@@ -17,14 +18,31 @@ bool timeTupleComp(const timeTuple& a, const timeTuple& b) {
 };
 
 std::vector<timeTuple> mergeRanges(std::vector<timeTuple> ranges) {
+	// Start by sorting the timeTuples
+	std::sort(ranges.begin(), ranges.end(), timeTupleComp);
 
-	for (auto it = begin(ranges); it != end(ranges); it++) {
-		true;
+	auto front = ranges.begin();
+	auto prev = ranges.begin();
+	for (auto curr = ranges.begin() + 1; curr != ranges.end(); curr++) {
+		if (prev->endTime > curr->startTime) {
+			// merge prev with curr
+			curr->startTime = prev->startTime;
+			front = curr;
+		}
 	}
+
+	return std::vector<timeTuple>(front, ranges.end());
 }
 
 
 int main() {
+
+	std::vector<timeTuple> ranges;
+	ranges.emplace_back(0,1);
+	ranges.emplace_back(3,5);
+	ranges.emplace_back(4,8);
+	ranges.emplace_back(10,12);
+	ranges.emplace_back(9,10);
 
 	return 0;
 }
