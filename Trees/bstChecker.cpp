ec5a2a9e747e7f84dff389c2d01f5dc9d3f9675f
michpasq <michpasq@umich.edu>
2015-07-12 19:05:39 -0400

    Made MAJOR bug fix by utilizing upper and lower bounds

--- Trees/bstChecker.cpp
+++ Trees/bstChecker.cpp
@@ -1,10 +1,17 @@
 #include <iostream>
 #include <stack>
+#include <limits>
+
+struct upperAndLower {
+    int upper = std::numeric_limits<int>::max();
+    int lower = std::numeric_limits<int>::min();
+};
 
 struct treeNode {
     int data;
     treeNode* left;
     treeNode* right;
+    upperAndLower bounds;
     treeNode(int a) : data(a), left(nullptr), right(nullptr) {};
 };
   
@@ -18,20 +25,36 @@ bool isBST(treeNode* root) {
         nodes.pop();
         
         if (current->left) {
-            if (current->left->data > current->data) {
-                std::cout << "short circuiting at " << current->data << ".\n";
+            if (current->left->data >= current->data) {
+                std::cout << "Short circuit at " << current->data << ".\t";
+                std::cout << "Left child is not less than current.\n";
                 return false;
             }
+            if (current->left->data < current->bounds.lower) {
+                std::cout << "Short circuit at " << current->data << ".\t";
+                std::cout << "Left child is less than lower bound on branch.\n";
+                return false;
+            }
+            current->left->bounds.lower = current->bounds.lower;
+            current->left->bounds.upper = current->data;
             nodes.push(current->left);
         }
         
         if (current->right) {
-            if (current->right->data < current->data) {
-                std::cout << "short circuiting at " << current->data << ".\n";
+            if (current->right->data <= current->data) {
+                std::cout << "Short circuit at " << current->data << ".\t";
+                std::cout << "Right child is not greater than current.\n";
                 return false;
             }
+            if (current->right->data > current->bounds.upper) {
+                std::cout << "Short circuit at " << current->data << ".\t";
+                std::cout << "Right child is greater than upper bound on branch.\n";
+                return false;
+            }
+            current->right->bounds.lower = current->data;
+            current->right->bounds.upper = current->bounds.upper;
             nodes.push(current->right);
-        }   
+        }
     }
     return true;
 }
@@ -71,6 +94,21 @@ int main() {
 */
     treeNode negOne = treeNode(-1);
     zero.right = &negOne;
+
+    std::cout << '\n';
+    isaBST = isBST(&one);
+    std::cout << (isaBST ? "Is BST" : "Is not BST") << "!\n";
+
+/*
+    Make another not BST
+        1
+       /  \
+      0    4
+          / \
+         -1   5
+*/
+    three.data = -1;
+    zero.right = nullptr;
 
     std::cout << '\n';
     isaBST = isBST(&one);
